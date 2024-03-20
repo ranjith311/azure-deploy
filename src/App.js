5161c046b7f3d9979df65f17dadb974be1fd44cd
Ranjith R <ranju8804@gmail.com>
2024-03-20 15:33:47 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Azure Deployment</h1> 
+        <h1>Azure Deployment Test</h1> 
       </header>
     </div>
   );
